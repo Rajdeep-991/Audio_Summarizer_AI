15920d4b9e6254783a0c964ed10270380cda09fd
Rajdeep Choudhury <151542010+Rajdeep-991@users.noreply.github.com>
2024-06-24 22:08:58 +0530

    Update Audio_Summarizer_AI.py

--- Audio Summarizer AI/Audio_Summarizer_AI.py
+++ Audio Summarizer AI/Audio_Summarizer_AI.py
@@ -1,11 +1,19 @@
+# Import the Whisper library for audio transcription
 import whisper
+
+# Import the custom module Text_Summarizer_AI, which contains the text summarization function
 import Text_Summarizer_AI
 
+# Load the Whisper model
 model = whisper.load_model("base")
-audio_file = input("Enter Audio File Name with Extension : ")
-result = model.transcribe(audio_file)
 
+# Prompt the user to input the audio file name
+audio_file = input("Enter Audio File Name with Extension : ")
+
+# Transcribe the audio file
+result = model.transcribe(audio_file)
 transcription = result["text"]
 
+# Print the summary of the transcription
 print()
 print(Text_Summarizer_AI.get_summary(transcription).replace('**', ''), "\n")

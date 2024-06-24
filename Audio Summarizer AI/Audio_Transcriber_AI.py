94d01ded0d72a21da2f48e2f206b14b34927d5fc
Rajdeep Choudhury <151542010+Rajdeep-991@users.noreply.github.com>
2024-06-24 22:02:46 +0530

    Update Audio_Transcriber_AI.py

--- Audio Summarizer AI/Audio_Transcriber_AI.py
+++ Audio Summarizer AI/Audio_Transcriber_AI.py
@@ -1,7 +1,12 @@
+# Import the Whisper library for audio transcription
 import whisper
 
+# Load the Whisper model
 model = whisper.load_model("base")
+
+# Transcribe the audio file
 result = model.transcribe("Sample_Audio_1.mp3")
 
+# Print the transcript
 print("\nTranscript\n----------")
 print(result["text"].strip(), "\n")
